01d7671e274aa84f3690bbe8662fb9095d8219c9
Jose Fernando Medina Sandoval <Jose.Fernando.Medina.Sandoval@ibm.com>
2022-09-15 12:44:58 -0500

    XD

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from translator import english_to_french, french_to_english
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertIsNone(english_to_french(""))
+    def test2(self):
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertIsNone(french_to_english(""))
+    def test2(self):
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+
+unittest.main()
